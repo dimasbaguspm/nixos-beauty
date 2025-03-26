81e98cb5a650f68c9621f1f26c3a48c2130f1c48
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-03-26 08:58:25 +0700

    chore: add fidget settings

--- home/programs/nixvim/plugins/fidget.nix
+++ home/programs/nixvim/plugins/fidget.nix
@@ -1,1 +1,17 @@
-{programs.nixvim.plugins = {fidget = {enable = true;};};}
+{
+  programs.nixvim.plugins = {
+    fidget = {
+      enable = true;
+      settings = {
+        notification = {
+          window = {
+            align = "top";
+            winblend = 0;
+            x_padding = 1;
+            y_padding = 1;
+          };
+        };
+      };
+    };
+  };
+}
