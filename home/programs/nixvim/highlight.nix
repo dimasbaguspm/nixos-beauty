9790ce45bb9d610c899c0cdb367e6a4584520530
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-03-29 14:11:26 +0700

    refactor(nixvim): change treesitter background to transparent

--- home/programs/nixvim/highlight.nix
+++ home/programs/nixvim/highlight.nix
@@ -1,7 +1,11 @@
 {
-  programs.nixvim.highlight = {
+  programs.nixvim.highlightOverride = {
     TreesitterContext = {
       sp = "NONE";
+    };
+    TreesitterContextBottom = {
+      sp = "#806d9c";
+      underline = true;
     };
   };
 }
