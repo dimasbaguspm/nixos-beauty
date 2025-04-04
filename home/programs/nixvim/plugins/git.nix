dc3b243afbda0d542d5f4c8d83f2e3ef777df5df
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-04 08:35:28 +0700

    feat(nixvim): toggle current line blame

--- home/programs/nixvim/plugins/git.nix
+++ home/programs/nixvim/plugins/git.nix
@@ -7,6 +7,12 @@
       gitsigns = {
         enable = true;
         lazyLoad.settings.event = "DeferredUIEnter";
+        settings = {
+          current_line_blame = true;
+          current_line_blame_opts = {
+            delay = 500;
+          };
+        };
       };
       diffview = {
         enable = true;
