a8682f46fc7767431b1b8d8eedb8c5297957ed60
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-04 21:33:53 +0700

    chore(kitty): change available background image

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -1,10 +1,29 @@
-{
+{pkgs, ...}: let
+  wallpapers = [
+    "red-1.png"
+    "kristin-lina-1.png"
+    "kristin-lina-2.jpg"
+    "kristin-lina-3.png"
+  ];
+
+  randomWallpaper = let
+    length = builtins.length wallpapers;
+    randomInt =
+      pkgs.runCommand "random-int" {
+        nativeBuildInputs = [pkgs.coreutils];
+      } ''
+        echo $(($RANDOM % ${toString length})) > $out
+      '';
+    index = builtins.fromJSON (builtins.readFile randomInt);
+  in
+    builtins.elemAt wallpapers index;
+in {
   programs.kitty = {
     enable = true;
     settings = {
       copy_on_select = true;
 
-      background_image = "~/images/wallpaper.jpg";
+      background_image = "~/images/${randomWallpaper}";
       background_opacity = "0.8";
       background_image_layout = "scaled";
       background_image_linear = true;
