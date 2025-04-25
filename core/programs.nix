ce9a6b4ee3ae0cd881b67e5da62ed24278a349a5
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-25 21:37:31 +0700

    feat(core): add vscode by default as a gui

--- core/programs.nix
+++ core/programs.nix
@@ -1,7 +1,21 @@
-{
+{pkgs, ...}: {
   programs = {
     git.enable = true;
     firefox.enable = true;
     zsh.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        github.copilot-chat
+        github.copilot
+        github.vscode-pull-request-github
+        esbenp.prettier-vscode
+        wix.vscode-import-cost
+        dbaeumer.vscode-eslint
+        yoavbls.pretty-ts-errors
+      ];
+    })
+  ];
 }
