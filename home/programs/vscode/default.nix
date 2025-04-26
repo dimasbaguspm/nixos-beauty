65075a57a8c891c3469719f951902f95f2e0d348
dimasbaguspm <dimas.bagus.pm1@gmail.com>
2025-04-26 21:42:36 +0700

    feat(vscode): add vscode under user dir

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -2,25 +2,40 @@
   programs.vscode = {
     enable = true;
     profiles = {
-      kyrielle = {
+      "kyrielle" = {
         userSettings = {
-          files.autoSave = "off";
-          editor = {
-            fontFamily = "'CaskaydiaCove Nerd Font', 'monospace', monospace";
-            minimap.enabled = false;
+          "files.autoSave" = "off";
+          "editor.fontFamily" = "'CaskaydiaCove Nerd Font', 'monospace', monospace";
+          "editor.defaultFormatter" = "dbaeumer.vscode-eslint";
+          "editor.formatOnSave" = true;
+          "editor.formatOnPaste" = true;
+          "editor.formatOnType" = false;
+          "editor.formatOnSaveMode" = "file";
+          "eslint.format.enable" = true;
+          "editor.codeActionsOnSave" = {
+            "source.fixAll.eslint" = true;
           };
         };
+        extensions = with pkgs.vscode-extensions;
+          [
+            yzhang.markdown-all-in-one
+            github.copilot-chat
+            github.copilot
+            dbaeumer.vscode-eslint
+            github.vscode-pull-request-github
+            wix.vscode-import-cost
+            yoavbls.pretty-ts-errors
+            streetsidesoftware.code-spell-checker
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "material-icon-theme";
+              publisher = "PKief";
+              version = "4.28.0";
+              sha256 = "sha256-DO3dcJPk+TMhfb0IJ/eTB7nIKfyCXIiyhZFBpZjJzsM=";
+            }
+          ];
       };
     };
-    extensions = with pkgs.vscode-extensions; [
-      yzhang.markdown-all-in-one
-      github.copilot-chat
-      github.copilot
-      github.vscode-pull-request-github
-      esbenp.prettier-vscode
-      wix.vscode-import-cost
-      dbaeumer.vscode-eslint
-      yoavbls.pretty-ts-errors
-    ];
   };
 }
